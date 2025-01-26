4531676fe6a42142d4df9ec4f9444b8a1aca8fb3
arkadiusoo <arkadiusz.mlyn@icloud.com>
2025-01-26 12:39:50 +0100

    data converted

--- dev.py
+++ dev.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import json
+import geopandas as gpd
+from shapely.geometry import Point
 
 # 1. Wczytaj dane z folderu data
 rent_data = pd.read_csv('data/apartments_rent_pl_2023_11.csv')
@@ -17,3 +19,18 @@ filtered_job_offers = []
 for offer in job_offers:
     if offer.get('city', '').lower() in cities:
         filtered_job_offers.append(offer)
+
+# Przekonwertuj dane na GeoDataFrame oraz przeprowadź projekcję CRS na EPSG:4326
+rent_gdf = gpd.GeoDataFrame(
+    filtered_rent_data,
+    geometry=gpd.points_from_xy(filtered_rent_data['longitude'],
+                                filtered_rent_data['latitude']),
+    crs='EPSG:4326'
+)
+job_gdf = gpd.GeoDataFrame(
+    filtered_job_offers,
+    geometry=[Point(offer['longitude'], offer['latitude'])
+              for offer in filtered_job_offers],
+    crs="EPSG:4326"
+)
+
